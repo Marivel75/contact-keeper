c6e8c7f83d2dcc4172956676b77284b06fe99d07
Mika <>
2021-01-24 23:28:01 +0100

    adjust gitignore

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,8 +1,8 @@
 import React, { Fragment, useContext, useEffect } from 'react';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ContactItem from './ContactItem';
-import ContactContext from '../../context/contact/contactContext';
 import Spinner from '../layouts/Spinner';
+import ContactContext from '../../context/contact/contactContext';
 
 const Contacts = () => {
     const contactContext = useContext(ContactContext);
